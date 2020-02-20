d4619df5303ea7359c1a56896ff56c18ccbd9ba9
JaspreetSingh Tuli <jaspreetsinghtuli@gmail.com>
2020-02-20 16:30:14 +0530

    Added keys.js

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+    gmailSender: {
+        user: 'your_email_here',
+        pass: 'your_password_here'
+    },
+    gmailReceivers : [
+        {
+            user: 'recipient'
+        }
+    ]
+};
